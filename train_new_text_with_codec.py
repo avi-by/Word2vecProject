fd4eb49327f96c47d6a5f0d05e6b52d29863b284
avi <aviby0@gmail.com>
2020-11-11 20:55:05 +0200

    readme change

--- train_new_text_with_codec.py
+++ train_new_text_with_codec.py
@@ -14,14 +14,14 @@ class SentenceIterator:
             yield line.split()
 
 
-def train(filepath,codec,savepath,sg=0,epochs=56,size=300,min_count=1,window=5,worker=4, hs=0, negative=5, ns_exponent=0.75,sample=1e-3):
+def train(filepath,codec,savepath,sg=0,epochs=56,size=300,min_count=10,window=5,worker=4, hs=0, negative=5, ns_exponent=0.75,sample=1e-3):
     print("strat")
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
     print("Current Time =", current_time)
     sentences = SentenceIterator(filepath, codec)
     # train your model  - make yourself cup of coffee or whats you like and wait
-    model = gensim.models.Word2Vec(sentences, sg=sg,epochs=epochs,size=size,min_count=min_count,window=window,workers=worker,hs=hs,negative=negative,ns_exponent=ns_exponent,sample=sample)
+    model = gensim.models.Word2Vec(sentences, sg=sg,iter=epochs,size=size,min_count=min_count,window=window,workers=worker,hs=hs,negative=negative,ns_exponent=ns_exponent,sample=sample)
     print("end")
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
